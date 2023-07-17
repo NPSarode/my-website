88861091bdd53091919deb0cf2e99d8c07900af5
Nikhil Sarode <138028934+NPSarode@users.noreply.github.com>
2023-07-17 20:51:50 +0530

    test1

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -3,31 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function MyProject() {
   return (
-    <div class="container-flex project mx-3 px-3   text-center">
+    <div className="container-flex project mx-3 px-3   text-center">
       {/* Start of Container div */}
 
-      <div class="row">
+      <div className="row">
         {/* Start of Row div */}
 
-        <div class="col my-3">
+        <div className="col my-3">
           {/* Start of Column div */}
 
-          <div class="card card1">
-            <div class="card-header">
+          <div className="card card1">
+            <div className="card-header">
               <h4>City Finder</h4>
             </div>
-            <div class="card-body">
-              <h6 class="card-title">
+            <div className="card-body">
+              <h6 className="card-title">
                 Finding the capital of the city using React Js.
               </h6>
-              <p class="card-text">Example of state props and routes.</p>
+              <p className="card-text">Example of state props and routes.</p>
             </div>
             <div className="butn my-3 ">
               <Link
                 to="https://find-capital.vercel.app/"
                 style={{ textDecoration: "none" }}
               >
-                <button type="button" class="btn btn-outline-light ">
+                <button type="button" className="btn btn-outline-light ">
                   View
                 </button>
               </Link>
@@ -36,7 +36,7 @@ export default function MyProject() {
                 to="https://github.com/NPSarode/capital-finder.git"
                 style={{ textDecoration: "none" }}
               >
-                <button type="button" class="btn btn-outline-light">
+                <button type="button" className="btn btn-outline-light">
                   Source Code
                 </button>
               </Link>
@@ -45,25 +45,25 @@ export default function MyProject() {
         </div>
         {/* End of Column div */}
 
-        <div class="col my-3">
+        <div className="col my-3">
           {/* Start of Column div */}
 
-          <div class="card card2">
-            <div class="card-header">
+          <div className="card card2">
+            <div className="card-header">
               <h4>Text Utility</h4>
             </div>
-            <div class="card-body">
-              <h6 class="card-title">
+            <div className="card-body">
+              <h6 className="card-title">
                 Performing different functionality on the text.
               </h6>
-              <p class="card-text">Example of state and props.</p>
+              <p className="card-text">Example of state and props.</p>
             </div>
             <div className="butn my-3 ">
               <Link
                 to="/"
                 style={{ textDecoration: "none" }}
               >
-                <button type="button" class="btn btn-outline-light ">
+                <button type="button" className="btn btn-outline-light ">
                   View
                 </button>
               </Link>
@@ -72,7 +72,7 @@ export default function MyProject() {
                 to="/"
                 style={{ textDecoration: "none" }}
               >
-                <button type="button" class="btn btn-outline-light">
+                <button type="button" className="btn btn-outline-light">
                   Source Code
                 </button>
               </Link>
